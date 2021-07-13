5e9812e5d6cba18904854834a687bd26ebf24d92
Alston Haynes <alston13@icloud.com>
2021-07-13 11:47:19 -0700

    DragonBallZ

--- js script.js
+++ js script.js
@@ -3,31 +3,51 @@ var x;
 x = prompt("Who's the main character of the show?");
 alert(" It's Goku!");
 var x;
-x=prompt("What's your favorite form?");
+x = prompt("What's your favorite form?");
 alert("I like that form too!")
-var x;
-x=prompt("How many Dragon Balls do you need to make a wish?");
-alert("You need 7 Dragon Balls!")
+function askshenron() {
+    var shenron = false;
+    while (shenron == false) {
+        shenron = prompt("How many Dragon Balls do you need to make a wish?");
+        if (shenron < 7) {
+            alert("Incorrect, try again.");
+            shenron = false;
 
-function askQuestion(){
+
+        }
+        else {
+            alert("Correct, you need 7 Dragon Balls!");
+
+        }
+    }
+}
+askshenron();
+
+
+//var x;
+//x=prompt("How many Dragon Balls do you need to make a wish?");
+//alert("You need 7 Dragon Balls!");
+
+function askQuestion() {
     var dbz = false;
-    while(dbz ==false){
-        dbz=prompt("How many do you have 0-7?");
-        if (dbz <7) {
+    while (dbz == false) {
+        var dragonballs = prompt("How many do you have 0-7?");
+        if (dragonballs < 7) {
             alert("You can't make a wish.");
-            dbz = true;    
-      }
-      else{
-          alert("All 7!? Go summon Shenron and make your wish!");
-      }
+           
+        }
+        else {
+            alert("All 7!? Go summon Shenron and make your wish!");
+            dbz = true;
+        }
     }
 
 }
-     
+
 askQuestion();
 
-var amount = prompt ("How many Dragon Balls do you want?");
-for(let i = 0; i < amount; i++){
+var amount = prompt("How many Dragon Balls do you want?");
+for (let i = 0; i < amount; i++) {
     document.write("<img src ='https://www.poopoopanda.com/assets/images/BAN85487-07.jpg'>");
 }
 
@@ -39,7 +59,7 @@ for(let i = 0; i < amount; i++){
 //if (dbz <"7") {
     //alert("You can't make a wish.");
     //dbz = true;
-    
+
 //}
 //else{
 
@@ -52,4 +72,4 @@ for(let i = 0; i < amount; i++){
 //var amount = prompt ("How many Dragon Balls do you want?");
 //for(let i = 0; i < amount; i++){
 //    document.write("<img src ='https://www.poopoopanda.com/assets/images/BAN85487-07.jpg>");
-//}
+
